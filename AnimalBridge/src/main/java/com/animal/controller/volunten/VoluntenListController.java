b620031fa40ca6f2a3d412afbc8be499de0deb39
Chocobe <nice_10bird@naver.com>
2019-11-20 16:54:20 +0900

    <Chocobe & JungMin> : Merged function CRUD at Volunten & Recruit

---
+++ AnimalBridge/src/main/java/com/animal/controller/volunten/VoluntenListController.java
@@ -0,0 +1,20 @@
+package com.animal.controller.volunten;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.animal.controller.SubController;
+
+public class VoluntenListController implements SubController{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		
+		
+	}
+
+}
