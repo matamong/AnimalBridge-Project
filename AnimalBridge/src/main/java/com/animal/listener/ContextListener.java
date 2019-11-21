4366345b9fca95c72712a9268885a8f8ae6dc26a
matamong <matamatamong@gmail.com>
2019-11-21 10:08:29 +0900

    Modify ContextListener.java Add MapControllers

--- AnimalBridge/src/main/java/com/animal/listener/ContextListener.java
+++ AnimalBridge/src/main/java/com/animal/listener/ContextListener.java
@@ -8,11 +8,13 @@ import javax.servlet.ServletContextListener;
 
 import com.animal.controller.SubController;
 import com.animal.controller.happyBoard.HappyBoardWriterController;
+import com.animal.controller.map.MapSaveController;
 import com.animal.controller.map.MapShowController;
 import com.animal.controller.member.MemberJoinController;
 import com.animal.controller.member.MemberLeaveController;
 import com.animal.controller.member.MemberLogInController;
 import com.animal.controller.member.MemberLogOutController;
+
 
 public class ContextListener implements ServletContextListener {
 	@Override
@@ -26,7 +28,8 @@ public class ContextListener implements ServletContextListener {
 		controllers.put("/view/memberView/memberLeave.do", new MemberLeaveController());
 		
 	// Map table
-		controllers.put("/view/MapView/MapTest.do", new MapShowController());
+		controllers.put("/view/mapView/Map.do", new MapShowController());
+		controllers.put("/view/mapView/SaveMap.do", new MapSaveController());
 		
 	// HappyBoard table
 		controllers.put("/view/happyBoardView/happyBoardWriter.do", new HappyBoardWriterController());
