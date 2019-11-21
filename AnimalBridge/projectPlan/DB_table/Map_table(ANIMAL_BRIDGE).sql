b74b3d3cf4f92286e22f0ec622d8f0bede36d439
matamong <matamatamong@gmail.com>
2019-11-21 10:10:00 +0900

    Modify Map Modify table map member_email -> member_nickname

--- AnimalBridge/projectPlan/DB_table/Map_table(ANIMAL_BRIDGE).sql
+++ AnimalBridge/projectPlan/DB_table/Map_table(ANIMAL_BRIDGE).sql
@@ -1,42 +1,28 @@
-CREATE USER scott IDENTIFIED BY tiger;  -- 이름 scott, 비밀번호 tiger 인 유저 생성
-GRNAT connect TO scott;
-GRANT all privileges TO scott;
 
-CREATE TABLE MEMBER(
-    EMAIL           VARCHAR2(30) PRIMARY KEY,
-    NAME            VARCHAR2(10) NOT NULL,
-    NICK_NAME       VARCHAR2(20) UNIQUE NOT NULL,
-    PASSWORD        VARCHAR2(20) NOT NULL,
-    PHONE           VARCHAR2(11) UNIQUE NOT NULL,
-    ADDR            VARCHAR2(30) NOT NULL,
-    EDU_STATE       VARCHAR2(5)  DEFAULT 'NO',
-    NO_SHOW_CNT     NUMBER(2)    DEFAULT 0,
-    GRADE           VARCHAR2(10) DEFAULT '직원'
-);
 
-INSERT INTO MEMBER(EMAIL, NAME, NICK_NAME, PASSWORD, PHONE, ADDR)
-            VALUES('a@a', 'kim', 'chocobe', 'pw', '010', '직원');
+INSERT INTO animal_member(EMAIL, NAME, NICK_NAME, PASSWORD, PHONE, ADDR)
+            VALUES('aa', 'aa', 'aa', 'aa', 'aa', '직원');
 
 SELECT * FROM MEMBER;
-            
-DROP TABLE MEMBER;
 
+
+-- MySql
 create table MAP(
-    email VARCHAR2(30),
-    x varchar2(25) not null,
-    y varchar2(25) not null,
-    map_address varchar2(50) primary key,
-    special varchar2(50),
-    title varchar2(15),
-    CONSTRAINT fk_map FOREIGN KEY (email)
-    REFERENCES member(email) ON DELETE CASCADE
+    member_nickname VARCHAR(30),
+    x VARCHAR(25) not null,
+    y VARCHAR(25) not null,
+    map_address VARCHAR(50) primary key,
+    special VARCHAR(50),
+    title VARCHAR(50),
+    FOREIGN KEY (member_nickname) REFERENCES animal_member (nick_name) 
+    on delete cascade
+    on update cascade
 );
 -- ON DELETE CASCADE : 부모테이블 값이 삭제되면 연쇄적으로 자식테이블 값 역시 삭제
+-- ON UPDATE CASCADE : 부모테이블 값이 수정되면 연쇄적으로 자식테이블 값 역시 수정
 
 INSERT INTO MAP 
-VALUES('a@a', '33.460001', '126.570770','어딘가','TEST','TEST');
+VALUES('aa', '126.57170473775234', '33.45065027408781','어딘가','TEST','TEST');
 
 SELECT * FROM MAP;
-
-ALTER TABLE map DROP CONSTRAINT fk_map;
-DROP TABLE map;
+drop table map;
