9b5186d7be7c82c957924c15d0065cf033a99c91
Chocobe <nice_10bird@naver.com>
2019-11-23 18:30:35 +0900

    <Chocobe> : Created class AnimalsDAO

---
+++ AnimalBridge/src/main/java/com/animal/dao/AnimalsDAO.java
@@ -0,0 +1,79 @@
+package com.animal.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import com.animal.dbConnection.DBCP;
+import com.animal.vo.AnimalsVO;
+
+public class AnimalsDAO {
+	private static final AnimalsDAO dao;
+	
+	private Connection conn;
+	private PreparedStatement preStatement;
+	private ResultSet resultSet;
+	
+	static {
+		dao = new AnimalsDAO();
+	}
+	
+	public static AnimalsDAO getInstance() {
+		return dao;
+	}
+	
+	
+	public int animalsWriter(AnimalsVO vo) {
+		int result = 0;
+		
+		try {
+			String sql = "INSERT INTO ANIMALS(CENTER_IDX, " +
+											 "ANIMAL_TYPE, " +
+											 "ANIMAL_SORT, " +
+											 "ANIMAL_GENDER, " +
+											 "ANIMAL_NEUTER, " +
+											 "ANIMAL_INOCULATION, " +
+											 "ANIMAL_FEATURE, " +
+											 "ADOPTION_DATE, " +
+											 "MEMBER_NICK_NAME, " +
+											 "IMG_1, " +
+											 "IMG_2, " +
+											 "IMG_3, " +
+											 "IMG_4, " +
+											 "IMG_5, " +
+											 "IMG_6) " +
+									  "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			conn = DBCP.getConnection();
+			
+			preStatement = conn.prepareStatement(sql);
+			preStatement.setInt(1, vo.getCenterIdx());
+			preStatement.setString(2, vo.getAnimalType());
+			preStatement.setString(3, vo.getAnimalSort());
+			preStatement.setString(4, vo.getAnimalGender());
+			preStatement.setString(5, vo.getAnimalNeuter());
+			preStatement.setString(6, vo.getAnimalInoculation());
+			preStatement.setString(7, vo.getAnimalFeature());
+			preStatement.setDate(8, Date.valueOf(LocalDate.now()));
+			preStatement.setString(9, vo.getMemberNickName());
+			preStatement.setString(10, vo.getImg_1());
+			preStatement.setString(11, vo.getImg_2());
+			preStatement.setString(12, vo.getImg_3());
+			preStatement.setString(13, vo.getImg_4());
+			preStatement.setString(14, vo.getImg_5());
+			preStatement.setString(15, vo.getImg_6());
+			
+			result = preStatement.executeUpdate();
+			
+		} catch(SQLException e) {
+			System.out.println("AnimalsDAO - animalsWriter() 에러 : " + e.getMessage());
+			
+		} finally {
+			DBCP.close(conn, preStatement);
+		}
+		
+		return result;
+	}
+}
